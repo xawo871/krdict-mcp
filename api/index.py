113aed77d6f005ce45160183011eaf534c069108
xawo871 <habibabdullah1w@gmail.com>
2026-09-22 00:50:01 +0700

    Initialize index.py and set up module path

---
+++ api/index.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+ROOT = os.path.dirname(
+    os.path.dirname(
+        os.path.abspath(__file__)
+    )
+)
+
+if ROOT not in sys.path:
+    sys.path.insert(0, ROOT)
+
+from server import app
